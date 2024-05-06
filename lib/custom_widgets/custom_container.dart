5b6803b18800abb255af277a809a7ccfc5efbbf5
Your Name <your_email@example.com>
2024-05-06 11:26:08 +0500

    fix splash color

--- lib/custom_widgets/custom_container.dart
+++ lib/custom_widgets/custom_container.dart
@@ -7,33 +7,39 @@ Widget customContainer({
   required String label,
   VoidCallback? onTap,
 }) {
-  return InkWell(
-    onTap: onTap,
-    child: Container(
-      height: height,
-      width: width,
-      decoration: BoxDecoration(
-        color: Colors.white10,
-        borderRadius: BorderRadius.circular(30),
-      ),
-      child: Column(
-        children: [
-          const SizedBox(height: 10),
-          Expanded(
-            flex: 3,
-            child: Image.asset(image, fit: BoxFit.contain),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Text(
-                label,
-                textAlign: TextAlign.center,
-                style: const TextStyle(color: Colors.grey),
+  return Material(
+    borderRadius: BorderRadius.circular(30),
+    color: Colors.transparent,
+    child: InkWell(
+      borderRadius: BorderRadius.circular(30),
+      onTap: onTap,
+      splashColor: Colors.white.withOpacity(0.3),
+      child: Container(
+        height: height,
+        width: width,
+        decoration: BoxDecoration(
+          color: Colors.white10,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            Expanded(
+              flex: 3,
+              child: Image.asset(image, fit: BoxFit.contain),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(
+                  label,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.grey),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     ),
   );
--- lib/custom_widgets/service_buttons.dart
+++ lib/custom_widgets/service_buttons.dart
@@ -1,36 +1,42 @@
-
 import 'package:flutter/material.dart';
 
-Widget serviceButtons(
-    {required double height,
-      required double width,
-      required String info,
-      required String image,
-      VoidCallback? onTap}) {
+Widget serviceButtons({
+  required double height,
+  required double width,
+  required String info,
+  required String image,
+  VoidCallback? onTap,
+}) {
   return Padding(
     padding: const EdgeInsets.all(8.0),
-    child: GestureDetector(
-      onTap: onTap,
-      child: Container(
-        height: height,
-        width: width,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          color: Colors.white10,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12.0),
-              child: Text(
-                info,
-                style: const TextStyle(color: Colors.white),
+    child: Material(
+      borderRadius: BorderRadius.circular(15),
+      color: Colors.transparent,
+      child: InkWell(
+        borderRadius: BorderRadius.circular(15),
+        onTap: onTap,
+        splashColor: Colors.white.withOpacity(0.3),
+        child: Container(
+          height: height,
+          width: width,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: Colors.white10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                child: Text(
+                  info,
+                  style: const TextStyle(color: Colors.white),
+                ),
               ),
-            ),
-            const Spacer(),
-            Image.asset(image)
-          ],
+              const Spacer(),
+              Image.asset(image),
+            ],
+          ),
         ),
       ),
     ),
